f7cbf43bafce5edb1957da6b13a83ddd0e4e9ca5
rafaelsolanogit config --global user.name rafaelsolanogit config --global user.name rafaelsolano <rafaelsolanol007@gmail.com>
2023-01-21 12:28:19 -0500

    Se crea el archivo de string template literals

---
+++ src/es6/strings.js
@@ -0,0 +1,17 @@
+let hello = 'hello'
+let world ='World'
+
+//Concarenar de forma obsoleta
+let epicpharse = hello + ' ' + world + ' !';
+console.log(epicpharse);
+
+//concatenar templetes literals
+let epicpharse2 =`${hello} ${world} !`
+console.log(epicpharse2);
+
+//Multiline es para poder trabajar con multiples lines 
+
+let lorem =`Esta es una frase epica
+con salto de linea con las comillas francesas`
+
+console.log(lorem);
